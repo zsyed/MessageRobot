421321a1b03a31233440cb2d9131c3b06ca43b6e
zsyed <zulfiqarsyed@yahoo.com>
2013-10-24 19:08:57 -0700

    Added MessageRobotDb class

---
+++ MessageRobot/MessageRobot/Models/MessageRobotDb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MessageRobot.Models
+{
+    public class MessageRobotDb: DbContext
+    {
+        public MessageRobotDb()
+            : base("DefaultConnection")
+        {
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
