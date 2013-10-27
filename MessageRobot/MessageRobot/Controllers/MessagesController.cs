8885a15a28d7b5f474123d067df2b8015ded8351
zsyed <zulfiqarsyed@yahoo.com>
2013-10-27 10:54:13 -0700

    Added page for Messages. Create, Edit, and Delete Message

--- MessageRobot/MessageRobot/Controllers/MessagesController.cs
+++ MessageRobot/MessageRobot/Controllers/MessagesController.cs
@@ -24,6 +24,53 @@ namespace MessageRobot.Controllers
             return HttpNotFound();
         }
 
+        [HttpGet]
+        public ActionResult Create(int contactId)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var model = _db.Messages.Find(id);
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var message = _db.Messages.Find(id);
+            _db.Messages.Remove(message);
+            _db.SaveChanges();
+            return RedirectToAction("Index", new { id = message.ContactId });
+        }
+
+        [HttpPost]
+        public ActionResult Create(Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Messages.Add(message);
+                _db.SaveChanges();
+                return RedirectToAction("Index", new {id = message.ContactId});
+            }
+            return View(message);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Messages.Add(message);
+                _db.Entry(message).State = System.Data.EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index", new { id = message.ContactId });
+            }
+            return View(message);
+        }
+
         protected override void Dispose(bool disposing)
         {
             _db.Dispose();
